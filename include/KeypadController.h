22d10a36a4c6a5b4bbfd066eabcc8fd7962d6fe1
jreue <jreue@users.noreply.github.com>
2025-11-08 15:18:17 -0600

    Created KeypadController

---
+++ include/KeypadController.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <Arduino.h>
+#include <Keypad.h>
+#include "hardware_config.h"
+
+class KeypadController {
+public:
+    KeypadController();
+    void begin();
+    char getKey();
+
+private:
+    Keypad keypad;
+    static byte rowPins[KEYPAD_ROW_COUNT];
+    static byte colPins[KEYPAD_COL_COUNT];
+    static char keyValues[KEYPAD_ROW_COUNT][KEYPAD_COL_COUNT];
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,28 @@
 #include <Arduino.h>
 #include <DIYables_4Digit7Segment_74HC595.h> 
 #include <LiquidCrystal_I2C.h>
-#include <Keypad.h>
 #include <ezBuzzer.h>
 
 #include "hardware_config.h"
 #include "GameEngine.h"
 #include "DateController.h"
 #include "LcdController.h"
+#include "KeypadController.h"
 
 LcdController lcdController(LCD_ADDRESS, LCD_COLUMNS, LCD_ROWS);
 GameEngine gameEngine;
+KeypadController keypadController;
 
 DateController dateController(D1_SCLK_PIN, D1_RCLK_PIN, D1_DIO_PIN,
                               D2_SCLK_PIN, D2_RCLK_PIN, D2_DIO_PIN);
 
-byte rowPins[KEYPAD_ROW_COUNT] = {KEYPAD_R1_PIN, KEYPAD_R2_PIN, KEYPAD_R3_PIN, KEYPAD_R4_PIN}; 
-byte colPins[KEYPAD_COL_COUNT] = {KEYPAD_C1_PIN, KEYPAD_C2_PIN, KEYPAD_C3_PIN};
-
-char keyValues[KEYPAD_ROW_COUNT][KEYPAD_COL_COUNT] = {
-  {'1','2','3'},
-  {'4','5','6'},
-  {'7','8','9'},
-  {'*','0','#'}
-};
-
-Keypad keypad = Keypad( makeKeymap(keyValues), rowPins, colPins, KEYPAD_ROW_COUNT, KEYPAD_COL_COUNT );
 ezBuzzer buzzer(BUZZER_PIN);
 
 void setup() {
   Serial.begin(SERIAL_BAUD_RATE);
   
   lcdController.begin();
+  keypadController.begin();
   
   dateController.begin();
   dateController.showDate(12, 25, 1975);
@@ -41,7 +32,7 @@ void loop() {
   dateController.loop();
   buzzer.loop();
   
-  char key = keypad.getKey();
+  char key = keypadController.getKey();
   if (key) {
     gameEngine.processKeyInput(key);
   }
